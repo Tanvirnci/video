853aff6712109117863e701bf898ae1b67b95200
Tanvirnci <tanvirhasanuk849@gmail.com>
2024-02-01 11:27:45 +0000

    first commit

---
+++ src/videotest/VideoApp.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package videotest;
+
+import java.util.jar.Attributes.Name;
+
+/**
+ *
+ * @author tanvi
+ */
+public class VideoApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    VideoGames v = new VideoGames ();
+    private String name = "game1";
+    v.setName(Name);
+    System.out.println("name: "+ v.getName());
+
+   
+
+    
+}
---
+++ src/videotest/VideoGames.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package videotest;
+
+/**
+ *
+ * @author tanvi
+ */
+public class VideoGames {
+    private String name;
+    private int id;
+
+    public VideoGames() {
+    }
+
+    public VideoGames(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+        
+}
